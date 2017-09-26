2973d64934ee1b0d8b7348d4de851937a2b63304
Norman Paniagua <normanpaniagua@gmail.com>
2017-09-26 19:16:45 +0200

    validate arguments

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const lib = require(`${__dirname}/build/Debug/binding.node`)
 
-module.exports = lib
+exports.backup2 = function(options, callback, progress) {
+	if (typeof options === 'function') {
+		if (typeof callback === 'function') progress = callback
+		callback = options
+		options = null
+	}
+
+	options = options || {}
+
+	if (options.uuid && !options.udid) options.udid = options.uuid
+
+	if (options.constructor.name !== 'Object') {
+		callback(Error('Options must be an object'), null)
+	} else if (typeof callback !== 'function') {
+		callback(Error('Callback is not a function'), null)
+	} else if (typeof progress !== 'function') {
+		callback(Error('Progress callback is not a function'), null)
+	} else {
+		return lib.idevice_backup2(options, callback, progress)
+	}
+}
+
+exports.info = function(options, callback) {
+	if (typeof options === "function") {
+		callback = options
+		options = null
+	}
+
+	options = options || {}
+
+	if (options.constructor.name !== 'Object') {
+		callback(Error('Options must be an object'), null)
+	} else if (typeof callback !== 'function') {
+		callback(Error('Callback is not a function'), null)
+	} else {
+		return lib.idevice_info(options, callback)
+	}
+}
