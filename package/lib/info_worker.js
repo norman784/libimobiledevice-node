b5deb2642e8dbc85281bd0bc39874b6079ebfbd1
Dídac Coll Pujals <didac.coll@qustodio.com>
2019-04-10 13:35:05 +0200

    Deleted package folder, added by mistake

--- package/lib/info_worker.js
+++
@@ -1,8 +0,0 @@
-const lib = require(`${__dirname}/../build/Release/imobiledevice.node`)
-
-process.on('message', options => {
-	lib.idevice_info(options, (err, data) => {
-		if (err) process.send({err: err})
-		else process.send({data: data})
-	})
-})
