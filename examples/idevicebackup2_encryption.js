63fc1165222c9a17c9aaf6b3df06b093fc611d46
Dídac Coll <didaccoll@gmail.com>
2021-06-02 00:01:44 +0200

    fix memory malloc when running encrypt command

--- examples/idevicebackup2_encryption.js
+++ examples/idevicebackup2_encryption.js
@@ -1,8 +1,8 @@
 const imobiledevice = require('../index');
 
 imobiledevice.backup2.encryption({ 
-    network: true,
-    encryption: {enable: true, password: '1234'}},
+    network: false,
+    encryption: {enable: false, password: '1234'}},
     (error, data) => {
     if (error) { console.error('encryption error: ', error); }
     else { console.log('encryption ', data) }
