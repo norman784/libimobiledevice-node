13d4948fef31b2dce37a272d712b7b6f0a5efe61
Dídac Coll <didaccoll@gmail.com>
2021-06-02 16:36:58 +0200

    fix libimobiledevice commit, fix release linker files

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-install_name_tool -change $PWD/dependencies/lib/libimobiledevice.6.dylib @loader_path/../../dependencies/lib/libimobiledevice.6.dylib $PWD/build/Release/imobiledevice.node
-install_name_tool -change $PWD/dependencies/lib/libplist.3.dylib @loader_path/../../dependencies/lib/libplist.3.dylib $PWD/build/Release/imobiledevice.node
+echo ""
+echo "Change absolute path to relative path"
+echo "-------------------------------------------"
+
+echo "[✔] imobiledevice.node"
+install_name_tool -change $PWD/dependencies/lib/libimobiledevice-1.0.6.dylib @loader_path/../../dependencies/lib/libimobiledevice-1.0.6.dylib $PWD/build/Release/imobiledevice.node
+install_name_tool -change $PWD/dependencies/lib/libplist-2.0.3.dylib @loader_path/../../dependencies/lib/libplist-2.0.3.dylib $PWD/build/Release/imobiledevice.node
+install_name_tool -change $PWD/dependencies/lib/libusbmuxd-2.0.6.dylib @loader_path/../../dependencies/lib/libusbmuxd-2.0.6.dylib $PWD/build/Release/imobiledevice.node
+
 echo "Done postinstall"
